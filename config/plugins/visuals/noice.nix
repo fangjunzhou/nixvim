9a88418c0fa5a93610ad3e31bb83d98cd7625886
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-07-01 03:36:16 -0500

    Added noice dismiss keymap

--- config/plugins/visuals/noice.nix
+++ config/plugins/visuals/noice.nix
@@ -1,6 +1,17 @@
+{ helpers, ... }:
 {
   plugins.noice = {
     enable = true;
     lsp.progress.enabled = false;
   };
+  keymaps = [
+    {
+      mode = [ "n" ];
+      key = "<leader>n";
+      action = "<cmd>Noice dismiss<cr>";
+      options = {
+        desc = "Dismiss Noice";
+      };
+    }
+  ];
 }
