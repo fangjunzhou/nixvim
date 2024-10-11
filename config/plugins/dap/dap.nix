7f389eef5aa47dd74a86a24908dea1b672416d1d
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-10-11 07:50:37 -0700

    Added python dap config

--- config/plugins/dap/dap.nix
+++ config/plugins/dap/dap.nix
@@ -13,6 +13,35 @@
           ];
         };
       };
+      servers = {
+        python = ''
+          function(cb, config)
+            if config.request == "attach" then
+              ---@diagnostic disable-next-line: undefined-field
+              local port = (config.connect or config).port
+              ---@diagnostic disable-next-line: undefined-field
+              local host = (config.connect or config).host or "127.0.0.1"
+              cb({
+                type = "server",
+                port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+                host = host,
+                options = {
+                  source_filetype = "python",
+                },
+              })
+            else
+              cb({
+                type = "executable",
+                command = os.getenv("VIRTUAL_ENV") .. "/bin/python",
+                args = { "-m", "debugpy.adapter" },
+                options = {
+                  source_filetype = "python",
+                },
+              })
+            end
+          end
+        '';
+      };
     };
     configurations = {
       java = [
@@ -35,6 +64,29 @@
           cwd = "\${workspaceFolder}";
           stopAtBeginningOfMainSubprogram = true;
           args = { };
+        }
+      ];
+      python = [
+        {
+          name = "Launch Python File";
+          type = "python";
+          request = "launch";
+          program = "\${file}";
+          pythonPath.__raw = ''
+            function()
+              -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+              -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+              -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+              local cwd = vim.fn.getcwd()
+              if vim.fn.executable(cwd .. '/venv/bin/python') == 1 then
+                return cwd .. '/venv/bin/python'
+              elseif vim.fn.executable(cwd .. '/.venv/bin/python') == 1 then
+                return cwd .. '/.venv/bin/python'
+              else
+                return '/usr/bin/python'
+              end
+            end
+          '';
         }
       ];
     };
