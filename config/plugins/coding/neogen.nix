fba310f208905b7a3b4b09b17206af4231e73496
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-01-06 22:40:44 -0800

    Changed python neogen convention to reST

--- config/plugins/coding/neogen.nix
+++ config/plugins/coding/neogen.nix
@@ -2,4 +2,17 @@
   plugins.neogen = {
     enable = true;
   };
+
+  extraConfigLua = ''
+    require("neogen").setup {
+      enabled = true,
+      languages = {
+        python = {
+          template = {
+            annotation_convention = "reST"
+          }
+        },
+      }
+    }
+  '';
 }
